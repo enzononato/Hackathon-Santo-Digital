1158f51ac60b9e406ff83f132446b632d18a9822
Enzo Nonato <enzononato10@gmail.com>
2024-08-29 23:25:51 -0300

    exercicio_1/main.py added

---
+++ desafio_0/exercicio_1/main.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("cls")
+
+def listaAsteristicos(n):
+    lista = []
+    
+    for i in range(n):
+        lista.append("*" * (i+1))
+    
+    return lista
+
+
+if __name__ == "__main__":
+    
+    listSize = int(input("Enter the list size: "))
+    
+    print(listaAsteristicos(listSize))
