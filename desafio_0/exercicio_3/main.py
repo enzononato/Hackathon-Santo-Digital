826efe60295274ff215ef23e36075a1b52cbff1d
Enzo Nonato <enzononato10@gmail.com>
2024-08-31 17:59:35 -0300

    updated desafio_0/exercicio_3/main.py

--- desafio_0/exercicio_3/main.py
+++ desafio_0/exercicio_3/main.py
@@ -2,8 +2,11 @@ import os
 
 os.system('cls')
 
-def gerar_subconjuntos(conjunto):   #Gera todos os subconjuntos 
+def gerar_subconjuntos(conjunto, max_size=float('inf'), min_size=0, distinct_only = False, sort_subsets= False):   #Gera todos os subconjuntos 
         
+    if distinct_only:
+        conjunto = list(set(conjunto)) #Transformo em 'set' para tirar as duplicatas
+    
     list_subset = [[]]
     
     for elemento in conjunto:
@@ -12,12 +15,50 @@ def gerar_subconjuntos(conjunto):   #Gera todos os subconjuntos
             novo_subconjunto = subset + [elemento]     # Cria um novo subconjunto adicionando o elemento atual    
             aux_list.append(novo_subconjunto)       # Adiciona o novo subconjunto à lista de novos subconjuntos
         list_subset.extend(aux_list)           # Adiciona os novos subconjuntos à lista principal
-   
+    
+    
+    if max_size >=0:
+        limited_list_size=[]
+        
+        for i in list_subset:
+            if len(i)<=max_size:
+                limited_list_size.append(i)
+        
+        list_subset = limited_list_size 
+    
+    if min_size:
+        
+        min_list_size = []
+        
+        for i in list_subset:
+            if len(i) >= min_size:
+                min_list_size.append(i)
+            list_subset = min_list_size
+        
+        """for i in min_list_size:
+            if i in list_subset:
+                list_subset.remove(i)"""
+                
+    if sort_subsets:
+        sorted_subsets = []
+        for i in list_subset:
+            sorted_subsets.append(sorted(i))
+        list_subset = sorted_subsets
+    
+        sorted_by_length =[]
+        
+        for i in range(len(list_subset) + 1):
+            for subset in list_subset:
+                if len(subset) == i:
+                    sorted_by_length.append(subset)
+        list_subset = sorted_by_length
+    
+    
     return list_subset
     
 
 if __name__ == "__main__":
-    conjunto = {1,2}
+    conjunto = [1,2]
     subconjuntos = gerar_subconjuntos(conjunto)
 
     print(subconjuntos)
